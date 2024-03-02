e556ce9ad3f32f851a0c0992cf5731a372046fa7
Martin Stefanoski <mstefano@1-D-16.42heilbronn.de>
2024-03-02 15:11:58 +0100

    header fixed

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mstefano <mstefano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:22:14 by mstefano          #+#    #+#             */
-/*   Updated: 2024/02/27 03:04:24 by mstefano         ###   ########.fr       */
+/*   Updated: 2024/03/02 15:04:17 by mstefano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,22 @@ typedef struct s_stack_node
 	struct s_stack_node	*next;
 	struct s_stack_node	*prev;
 }	t_stack_node;
+
+int		main(int argc, char **argv);
+void	rra(t_stack_node **stack_a);
+void	rrb(t_stack_node **stack_b);
+void	rrr(t_stack_node **stack_a, t_stack_node **stack_b);
+void	ra(t_stack_node **stack_a);
+void	rb(t_stack_node **stack_b);
+void	rr(t_stack_node **stack_a, t_stack_node **stack_b);
+void	pa(t_stack_node **stack_a, t_stack_node **stack_b);
+void	pb(t_stack_node **stack_a, t_stack_node **stack_b);
+void	sa(t_stack_node **stack_a);
+void	sb(t_stack_node **stack_b);
+void	ss(t_stack_node **stack_a, t_stack_node **stack_b);
+void	error(void);
+void	*create_stack(int argc, char **argv, t_stack_node *stack);
+int		*is_sorted(t_stack_node *stack);
+void	*sort_stack(t_stack_node **stack_a, t_stack_node **stack_b);
 
 #endif // PUSH_SWAP_H
